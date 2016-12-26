3d87af546968107c783361f134907eb68d74393b
collinl34 <collinlshelby@gmail.com>
2016-12-25 23:38:43 -0800

    categories model create

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20161226073344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "address"
+    t.integer  "zip_code"
+    t.string   "city"
+    t.string   "state"
+    t.integer  "apt"
+    t.string   "images"
+    t.string   "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
