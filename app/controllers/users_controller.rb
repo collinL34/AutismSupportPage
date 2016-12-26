d317b7419261063cddc749f332bbe8bded78d6a3
collinl34 <collinlshelby@gmail.com>
2016-12-25 23:54:34 -0800

    user seed addition to data

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
   def index
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :address, :zip_code, :city, :state, :email, :password)
+  end
 end
