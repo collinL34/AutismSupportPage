6e46955c096f160363854638c781cab40b2fa60e
collinl34 <collinlshelby@gmail.com>
2016-12-25 23:46:22 -0800

    articles migration create

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226073726) do
+ActiveRecord::Schema.define(version: 20161226073942) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "articles", force: :cascade do |t|
+    t.string   "body"
+    t.integer  "category_id"
+    t.integer  "comment_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string   "name"
