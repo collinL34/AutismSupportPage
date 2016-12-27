7cd0bb27c92387a49874f7316961154127b23df2
collinl34 <collinlshelby@gmail.com>
2016-12-27 14:49:23 -0800

    controllers create for app

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,21 @@ class UsersController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def show
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
   private
 
   def user_params
