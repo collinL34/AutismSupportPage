13a7df02de85fd719fe45bdfe4ccf38e9bae080d
collinl34 <collinlshelby@gmail.com>
2017-01-02 18:19:33 -0800

    merge conflict fix

--- app/assets/javascripts/controller/articles_controller.js
+++ app/assets/javascripts/controller/articles_controller.js
@@ -1,13 +1,4 @@
-// var app = angular.module('angularRails', ['$scope']);
+var app = angular.module('angularRails', ['$scope']);
 
-<<<<<<< HEAD
 app.directive('articles-list', function($scope) {
-    $scope.addArticle = function() {
-    };
 });
-=======
-// app.directive('articles-list', function($scope) {
-//     $scope.addArticle = function() {
-//     };
-// });
->>>>>>> f44672790528b30b002777a78b6ce41a4c42f05c
