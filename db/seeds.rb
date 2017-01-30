c567001e4d8f8fd6bd2b4acca1d195968c964ff0
collinl34 <collinlshelby@gmail.com>
2017-01-30 13:57:56 -0800

    data add to corrected seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,22 +11,21 @@ Category.create(name: 'inspirational')
 Category.create(name: 'financial')
 Category.create(name: 'support')
 
-# art1 = Article.create(category_id: 1, link: '', author_id: 1)
-# art4 = Article.create(category_id: 2, link: , author_id: 1)
-# art5 = Article.create(category_id: 5, link: , author_id: 1)
-# art6 = Article.create(category_id: 2, link: , author_id: 1)
-# art7 = Article.create(category_id: 2, link: , author_id: 1)
-# art8 = Article.create(category_id: 6, link: , author_id: 1)
+art1 = Article.create(category_id: 1, link: 'https://www.ninds.nih.gov/Disorders/Patient-Caregiver-Education/Fact-Sheets/Autism-Spectrum-Disorder-Fact-Sheet', author_id: 1)
+art3 = Article.new(category_id: 1, link: 'https://www.autismspeaks.org/what-autism', author_id: 1)
+art4 = Article.create(category_id: 2, link: 'https://www.autismspeaks.org/node/314251', author_id: 1)
+art5 = Article.create(category_id: 5, link: 'https://www.autismspeaks.org/advocacy/advocacy-news/autism-speaks-legislative-wins-2016', author_id: 1)
+art6 = Article.create(category_id: 2, link: 'https://www.autismspeaks.org/science/resources-programs/autism-treatment-network', author_id: 1)
+art7 = Article.create(category_id: 2, link: 'https://iancommunity.org/', author_id: 1)
+art8 = Article.create(category_id: 6, link: 'https://takesbrains.org/', author_id: 1)
 
-# articles = [art1, art2, art3, art4, art5, art6, art7, art8]
+articles = [art1, art2, art3, art4, art5, art6, art7, art8]
 
-# articles.each do |article| 
-  a.body    = Article.text_grabber(nokogiri_obj) 
-  a.image_tag = Article.img_grabber(nokogiri_obj)
-# end
-
-# Friendship.create(user_id: 1, friend_id: 2)
+articles.each do |article| 
+  nokogiri_obj    = Article.nokogiri_obj_makr(article.link)
   article.title   = Article.title_grabber(nokogiri_obj) 
+  article.body    = Article.text_grabber(nokogiri_obj) 
+  article.img_tag = Article.img_grabber(nokogiri_obj)
+end
 
-a = Article.new(category_id: 1, link: 'http://www.dw.com/en/child-autism-diagnosis-in-vogue-in-germany/a-37306457', author_id: 1)
-nokogiri_obj    = Article.nokogiri_obj_makr(a.link)
+Friendship.create(user_id: 1, friend_id: 2)
