8fab7d9cdf9d588134674e0cfb4f0ed4085c5a70
collinl34 <collinlshelby@gmail.com>
2017-01-30 12:10:31 -0800

    methods add to article model for parsing links

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,25 @@
 class Article < ApplicationRecord
   belongs_to :category, class_name: 'Category'
   belongs_to :author, class_name: 'User'
+
+  def white_space_cleaner(html_text)
+    html_text.gsub(/\s+/, ' ')
+  end
+
+  def nokogiri_obj_makr(html_link) 
+    Nokogiri::HTML(open("#{html_link}"))
+  end
+
+  def title_parse(html_doc)
+    html_doc.css('title').first
+  end
+
+  def img_grabber(html_doc)
+    html_doc.css('img').first['src']
+  end
+
+  def text_grabber(html_doc)
+    white_space_cleaner(html_doc.css('p').text)
+  end
+
 end
