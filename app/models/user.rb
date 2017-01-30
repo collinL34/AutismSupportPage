b9d099343fa30259da4087c987d5745fbdb776de
collinl34 <collinlshelby@gmail.com>
2017-01-30 12:30:16 -0800

    awesome_print add to gemfile along with assoc test with articles and users functining passing

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
 
-  has_many :posts, foreign_key: 'author_id'
+  has_many :articles, foreign_key: 'author_id'
   has_many :friendships
   has_many :friends, through: :friendships
   has_many :comments, as: :commentable
