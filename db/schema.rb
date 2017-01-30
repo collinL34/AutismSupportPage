c5703907d6f1308396c41980a928db2dc6aa9842
collinl34 <collinlshelby@gmail.com>
2017-01-30 12:19:00 -0800

    :sparkles: post table remove due to it lack of need

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161226191317) do
+ActiveRecord::Schema.define(version: 20170130201613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -51,15 +51,6 @@ ActiveRecord::Schema.define(version: 20161226191317) do
   create_table "images", force: :cascade do |t|
     t.string   "link"
     t.integer  "owner_id"
-    t.integer  "comment_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "posts", force: :cascade do |t|
-    t.string   "body"
-    t.string   "link"
-    t.integer  "author_id"
     t.integer  "comment_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
