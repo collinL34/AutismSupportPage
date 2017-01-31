56050a343a0d11388589b4d3e7924b8a9bb47d1c
collinl34 <collinlshelby@gmail.com>
2017-01-30 16:43:05 -0800

    seed data bug fix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,7 @@ class HomeController < ApplicationController
 
   def index
     @user = User.new
+    @article = Article.first
   end
   
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,13 +11,13 @@ Category.create(name: 'inspirational')
 Category.create(name: 'financial')
 Category.create(name: 'support')
 
-art1 = Article.create(category_id: 1, link: 'https://www.ninds.nih.gov/Disorders/Patient-Caregiver-Education/Fact-Sheets/Autism-Spectrum-Disorder-Fact-Sheet', author_id: 1)
+art1 = Article.new(category_id: 1, link: 'https://www.ninds.nih.gov/Disorders/Patient-Caregiver-Education/Fact-Sheets/Autism-Spectrum-Disorder-Fact-Sheet', author_id: 1)
 art2 = Article.new(category_id: 1, link: 'https://www.autismspeaks.org/what-autism', author_id: 1)
-art3 = Article.create(category_id: 2, link: 'https://www.autismspeaks.org/node/314251', author_id: 1)
-art4 = Article.create(category_id: 5, link: 'https://www.autismspeaks.org/advocacy/advocacy-news/autism-speaks-legislative-wins-2016', author_id: 1)
-art5 = Article.create(category_id: 2, link: 'https://www.autismspeaks.org/science/resources-programs/autism-treatment-network', author_id: 1)
-art6 = Article.create(category_id: 2, link: 'https://iancommunity.org/', author_id: 1)
-art7 = Article.create(category_id: 6, link: 'https://takesbrains.org/', author_id: 1)
+art3 = Article.new(category_id: 2, link: 'https://www.autismspeaks.org/node/314251', author_id: 1)
+art4 = Article.new(category_id: 5, link: 'https://www.autismspeaks.org/advocacy/advocacy-news/autism-speaks-legislative-wins-2016', author_id: 1)
+art5 = Article.new(category_id: 2, link: 'https://www.autismspeaks.org/science/resources-programs/autism-treatment-network', author_id: 1)
+art6 = Article.new(category_id: 2, link: 'https://iancommunity.org/', author_id: 1)
+art7 = Article.new(category_id: 6, link: 'https://takesbrains.org/', author_id: 1)
 
 articles = [art1, art2, art3, art4, art5, art6, art7]
 
@@ -26,6 +26,7 @@ articles.each do |article|
   article.title      = Article.title_grabber(nokogiri_obj) 
   article.body       = Article.text_grabber(nokogiri_obj) 
   article.image_link = Article.img_grabber(nokogiri_obj)
+  article.save
 end
 
 Friendship.create(user_id: 1, friend_id: 2)
