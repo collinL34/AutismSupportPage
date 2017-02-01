9b842d1bb51ec5e227289706f6783c55fbfc3b05
collinl34 <collinlshelby@gmail.com>
2017-01-31 23:24:59 -0800

    :sparkles: user sign up implemented

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,14 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user  = User.new(params[:user_params])
+    @user  = User.new(user_params)
+
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
+    else
+      redirect_to :back
+    end
   end
 
   def update
