4838b1a35a6f1e0a3de8cced37ceebe61d4edbd7
collinl34 <collinlshelby@gmail.com>
2017-02-21 20:50:19 -0800

    :sparkles: friend model method add_friend method create and friends logic add to user show

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,14 @@
 class Friendship < ApplicationRecord
   belongs_to :user
   belongs_to :friend, class_name: 'User'
+
+  def self.add_friend(current_user, new_friend)
+    p '******************************'
+    if current_user.id != new_friend.id
+      current_user.friendships.create(user_id: current_user.id, friend_id: new_friend.id)
+    else
+      p "Cant be the same user!"
+    end
+  end
+
 end
