050da2581d877cac65b9dce51f8f978cf6741451
collinl34 <collinlshelby@gmail.com>
2017-02-24 23:43:18 -0800

    :sparkles: article add feature implement

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,34 @@
 class ArticlesController < ApplicationController
-  def index
-    @articles = Article.all
+
+  def index    
   end
+
+  def new
+    @article = Article.new()
+  end
+
+  def edit
+  end
+
+  def create
+    obj = Article.nokogiri_obj_makr(params[:article][:link])
+    title = Article.title_grabber(obj)
+    img = Article.img_grabber(obj)
+    categ = Category.find_by_name(params[:article][:category]).id
+
+    @article  = Article.new(title: title, category_id: categ, image_link: img, author_id: session[:user_id])
+
+    if @article.save
+      redirect_to :root
+    else
+      redirect_to :back
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
 end
