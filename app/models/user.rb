ed3a95e2e7d49dfb1847f75f7e84a595e2af835a
collinl34 <collinlshelby@gmail.com>
2017-03-03 14:05:41 -0800

    :sparkles: user login and new form errors add for better ux

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  validates_numericality_of :zip_code
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
   has_secure_password
 
   has_many :articles, foreign_key: 'author_id'
