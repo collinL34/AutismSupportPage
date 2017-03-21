5ed5e942ecc1a198321fb6e09ca2d0a7bc4a5fa3
collinl34 <collinlshelby@gmail.com>
2017-03-21 15:50:47 -0700

    :construction: more test add and are passing for home controller except one pending search query in production, gitignore dev.log add

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+  describe 'get #index' do 
+
+    it 'responds with a 200 status code' do
+      get :index
+      expect(response).to have_http_status 200
+    end
+
+    it 'renders the index template' do
+      get :index
+      expect(response).to render_template(:index)
+    end
+
+    it 'assigns all articles to @articles' do
+      get :index
+      expect(assigns(:articles)).to eq(Article.all)
+    end
+
+    it 'returns the correct articles by category'
+
+  end
+
+end
