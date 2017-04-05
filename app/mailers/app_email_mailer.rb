d3b331e7a852cebc0d3aa3e73ab9e6120459bd47
collinl34 <collinlshelby@gmail.com>
2017-04-04 21:16:26 -0700

    bug remove from category search results card now no longer visable when no search has been declared

--- app/mailers/app_email_mailer.rb
+++ app/mailers/app_email_mailer.rb
@@ -1,9 +1,2 @@
 class AppEmailMailer < ApplicationMailer
-  default from: 'collinlshelby@gmail.com'
-
-  def email(user)
-    # @user = user
-    mail(to: user, subject: 'hello this is a test')
-  end
-
 end
